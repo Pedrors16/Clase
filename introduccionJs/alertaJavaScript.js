f197fd3467f191fd69572ff71db2d682b7db10a1
Pedrors16 <pedrors16asturias@gmail.com>
2022-01-14 12:16:00 +0100

    lololo

---
+++ introduccionJs/alertaJavaScript.js
@@ -0,0 +1,7 @@
+alert ('DjPako');
+document.write ('Hola wenas');
+console.log ('Sergio bobo')
+confirm ('DjPako');
+//esto es un comentario de una linea
+/* esto es un comentario
+de dos o mas lineas*/
