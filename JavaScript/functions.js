d44bcddff5fd961d112355731697e11b06d1d844
Pedrors16 <pedrors16asturias@gmail.com>
2022-02-08 12:39:17 +0100

    lololo

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -2,7 +2,8 @@
 document.getElementById('grayButton').onclick = switchGray;
 document.getElementById('whiteButton').onclick = switchWhite;
 document.getElementById('blueButton').onclick = switchBlue;
-
+document.getElementById('yellowButton').onclick = switchYellow;
+document.getElementById('greenButton').onclick = switchGreen;
 
 //Cambia la fuente y el fondo de pantalla
 function switchGray(){
@@ -18,4 +19,13 @@ function switchBlue(){
   document.getElementsByTagName('body')[0].style.color = 'white';
 }
 
+function switchYellow(){
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'yellow';
+  document.getElementsByTagName('body')[0].style.color = 'black';
+}
+
+function switchGreen(){
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'green';
+  document.getElementsByTagName('body')[0].style.color = 'black';
+}
 //Añade el código necesario para que al pulsar el botón amarillo el fondo sea amarillo y la fuente verde. 
